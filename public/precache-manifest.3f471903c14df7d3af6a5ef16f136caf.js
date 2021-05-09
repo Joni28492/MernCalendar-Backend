39990cee74b3f79dd10a11c383eccbbe4d411ab8
Joni <joni28492@gmail.com>
2021-05-09 21:48:11 +0200

    version 1 del Calendar

---
+++ public/precache-manifest.3f471903c14df7d3af6a5ef16f136caf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2899af7144f3a680dd8e3f1ce14bcb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7fbddd7fb74fae7e879",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "8593259a415a3e320fd2",
+    "url": "/static/css/main.b2bf33be.chunk.css"
+  },
+  {
+    "revision": "b7fbddd7fb74fae7e879",
+    "url": "/static/js/2.67df9888.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.67df9888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8593259a415a3e320fd2",
+    "url": "/static/js/main.eea1999d.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
